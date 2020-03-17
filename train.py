172619f1ff25314a47ce59610337363d858d8ed5
JyotiSunkara <jyoti.sunkara@students.iiit.ac.in>
2020-03-18 01:18:38 +0530

    Teeny comment changes

--- train.py
+++ train.py
@@ -2,8 +2,6 @@ from client_moodle import get_errors, submit
 import numpy as np
 import random 
 
-
-
 '''
 1. Randomly initialize populations P
 2. Determine fitness of population
@@ -28,7 +26,7 @@ class Item(object):
     fitness must be least, that is zero'''
     def calcFitness(self): 
         error = get_errors('dnLVLTHPAUOT2R1Ruj1sQvXxWBZZchp8u4WkyZGzaeTQCpyFXC', self.chromosome)
-        '''Validation set error'''
+        '''Returning validation set error as fitness'''
         return error[1] 
         # 2) Idk what we are supposed to do with training set error
    
@@ -64,14 +62,17 @@ def main():
         '''Sort by fitness'''
         population = sorted(population, key = lambda x:x.fitness) 
 
+        #3) Not sure if fitness should be equivalent to validation set error and also if error can infact reach 0. Must check.
         if population[0].fitness <= 0:
             found =  True
             break
-
+        
+        '''10 percent directly enters new generation'''
         newGeneration = []
         entry = int((10*POPULATION_SIZE)/100) 
         newGeneration.extend(population[:entry])
-
+        
+        '''Top 50 percent mates to get new generation'''
         mates = int((90*POPULATION_SIZE)/100) 
         for X in range(mates):
             parentOne = random.choice(population[:50])
@@ -89,5 +90,5 @@ def main():
 if __name__ == '__main__':
     main()
     
-#3) Idk how to continue learning over all the days we have, as in we should be storing results 
+#4) Idk how to continue learning over all the days we have, as in we should be storing results 
 # so that the vector becomes better right? We should probably keep the last population in a file? 

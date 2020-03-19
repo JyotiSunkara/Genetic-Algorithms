703d80c4748faaef719f26153b2639ddcacfa70f
Shradha <shradha.sgl@gmail.com>
2020-03-19 20:50:43 +0530

    Progress

--- new.py
+++ new.py
@@ -4,7 +4,7 @@ import random
 from key import SECRET_KEY 
 from csv import DictWriter
 
-POPULATION_SIZE = 3
+POPULATION_SIZE = 100
 VECTOR_SIZE = 11
 MATING_POOL_SIZE = 20
 first_parent = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
@@ -20,7 +20,7 @@ def initial_population():
 def calculate_fitness(population):
     fitness = np.empty(POPULATION_SIZE)
 
-    j = 10
+    j = 100
     for i in range(POPULATION_SIZE):
         # error = get_errors(SECRET_KEY, list(population[i]))
         # fitness[i] = error[0]*0.7 + error[1]
@@ -64,12 +64,18 @@ def new_generation(parents_fitness, children):
     generation = np.concatenate(parents_fitness, children_fitness)
     return generation
 
-population = initial_population()
-population_fitness = calculate_fitness(population)
 
-num_generations = 5
-for generation in range(num_generations):   
-    
-    mating_pool = mating_pool(population_fitness)
-    children = create_children(mating_pool)
-    population_fitness = new_generation(mating_pool, children)
+
+def main():
+    population = initial_population()
+    population_fitness = calculate_fitness(population)
+
+    num_generations = 3
+    for generation in range(num_generations):   
+        
+        mating_pool = mating_pool(population_fitness)
+        children = create_children(mating_pool)
+        population_fitness = new_generation(mating_pool, children)
+
+if __name__ == '__main__':
+    main()

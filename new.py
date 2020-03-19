c6c44e3ecfecf9a3fa084b12d1f9a27c15005a72
Shradha <shradha.sgl@gmail.com>
2020-03-19 20:48:05 +0530

    Progress

---
+++ new.py
@@ -0,0 +1,75 @@
+from client_moodle import get_errors, submit
+import numpy as np
+import random 
+from key import SECRET_KEY 
+from csv import DictWriter
+
+POPULATION_SIZE = 3
+VECTOR_SIZE = 11
+MATING_POOL_SIZE = 20
+first_parent = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
+
+def initial_population():
+    first_population = [np.copy(first_parent) for i in range(POPULATION_SIZE)]
+    for i in range(POPULATION_SIZE-1):
+        index = random.randint(0,10)
+        first_population[i][index] = random.uniform(-10,10)
+
+    return first_population
+
+def calculate_fitness(population):
+    fitness = np.empty(POPULATION_SIZE)
+
+    j = 10
+    for i in range(POPULATION_SIZE):
+        # error = get_errors(SECRET_KEY, list(population[i]))
+        # fitness[i] = error[0]*0.7 + error[1]
+        fitness[i] = j
+        j-=1
+
+    pop_fit = np.column_stack((population, fitness))
+    pop_fit = pop_fit[np.argsort(pop_fit[:,-1])]
+    return pop_fit
+
+def mating_pool(population_fitness):
+    mating_pool = population_fitness[:MATING_POOL_SIZE]
+    return mating_pool
+
+def mutation(child):
+    mutation_index = random.randint(0, VECTOR_SIZE)
+    child[mutation_index] = random.uniform(-10,10)
+    return child
+
+def crossover(parent1, parent2):
+    crossover_point = random.randint(0, VECTOR_SIZE)
+    child = parent1[:crossover_point] + parent2[crossover_point:]
+    return child
+
+def create_children(mating_pool):
+
+    mating_pool = mating_pool[:, :-1]
+    children = []
+    for i in range(POPULATION_SIZE):
+        parent1 = mating_pool[random.randint(0, MATING_POOL_SIZE)]
+        parent2 = mating_pool[random.randint(0, MATING_POOL_SIZE)]
+        child = crossover(parent1, parent2)
+        child = mutation(child)
+        children.append(child)
+
+    return children 
+
+def new_generation(parents_fitness, children):
+    children_fitness = calculate_fitness(children)
+    children_fitness = children_fitness[:80]
+    generation = np.concatenate(parents_fitness, children_fitness)
+    return generation
+
+population = initial_population()
+population_fitness = calculate_fitness(population)
+
+num_generations = 5
+for generation in range(num_generations):   
+    
+    mating_pool = mating_pool(population_fitness)
+    children = create_children(mating_pool)
+    population_fitness = new_generation(mating_pool, children)
--- train.py
+++ train.py
@@ -1,19 +1,11 @@
 from client_moodle import get_errors, submit
 import numpy as np
 import random 
-
-'''
-1. Randomly initialize populations P
-2. Determine fitness of population
-3. Until convergence repeat:
-      a. Select parents from population
-      b. Crossover and generate new population
-      c. Perform mutation on new population
-      d. Calculate fitness for new population
-'''
-
+from key import SECRET_KEY 
 from csv import DictWriter
  
+first_parent = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
+
 def appendDict(fileName, dictElements, fieldNames):
     
     with open(fileName, 'a+', newline='') as writeObj:
@@ -23,24 +15,16 @@ def appendDict(fileName, dictElements, fieldNames):
 fieldNames = ['Generation','Vector','Fitness']
 POPULATION_SIZE = 100
 
-# 1) Idk how we should be 
-#   using the fact that the vector given is overfit
-
 class Item(object):
 
     def __init__(self, chromosome): 
         self.chromosome = chromosome  
         self.fitness = self.calcFitness() 
 
-    '''Target is unknown, but when target is reached 
-    fitness must be least, that is zero'''
     def calcFitness(self): 
-        error = get_errors('dnLVLTHPAUOT2R1Ruj1sQvXxWBZZchp8u4WkyZGzaeTQCpyFXC', self.chromosome.tolist())
-        '''Returning validation set error as fitness'''
+        error = get_errors(SECRET_KEY, self.chromosome.tolist())
         return error[1] 
-        # 2) Idk what we are supposed to do with training set error
    
-'''Valid numbers are in the range -10 to 10'''
 def create_genome():
     genome = np.random.uniform(-10, 10, size = 11)
     return genome
@@ -61,7 +45,6 @@ def main():
     global POPULATION_SIZE
     generation = 1
     
-    '''List of 100 items where each Item is a vector and its fitness'''
     population = [] 
 
     for X in range(POPULATION_SIZE):
@@ -69,20 +52,16 @@ def main():
         population.append(genome)
     found = False
     while not found and generation != 1499:
-        '''Sort by fitness'''
         population = sorted(population, key = lambda x:x.fitness) 
 
-        #3) Not sure if fitness should be equivalent to validation set error and also if error can infact reach 0. Must check.
         if population[0].fitness <= 0:
             found =  True
             break
         
-        '''10 percent directly enters new generation'''
         newGeneration = []
         entry = int((10*POPULATION_SIZE)/100) 
         newGeneration.extend(population[:entry])
         
-        '''Top 50 percent mates to get new generation'''
         mates = int((90*POPULATION_SIZE)/100) 
         for X in range(mates):
             parentOne = random.choice(population[:50])
@@ -98,12 +77,10 @@ def main():
 
     rowDict = {'Generation': generation, 'Vector': population[0].chromosome, 'Fitness': population[0].fitness}
     appendDict('store.csv', rowDict, fieldNames)
-    submit_status = submit('dnLVLTHPAUOT2R1Ruj1sQvXxWBZZchp8u4WkyZGzaeTQCpyFXC', population[0].chromosome.tolist())
+    submit_status = submit(SECRET_KEY, population[0].chromosome.tolist())
     assert "submitted" in submit_status
 
 
 if __name__ == '__main__':
     main()
     
-#4) Idk how to continue learning over all the days we have, as in we should be storing results 
-# so that the vector becomes better right? We should probably keep the last population in a file? 

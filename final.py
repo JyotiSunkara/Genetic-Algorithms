6f3d6fca0688a07f133397a25ed53f17198f9df3
JyotiSunkara <jyoti.sunkara@students.iiit.ac.in>
2020-03-23 23:50:25 +0530

    Submit script

--- final.py
+++ final.py
@@ -8,7 +8,7 @@ import os
 POPULATION_SIZE = 100
 VECTOR_SIZE = 11
 MATING_POOL_SIZE = 10
-FILE_NAME = 'JSON/shradha.json'
+FILE_NAME = 'JSON/test.json'
 first_parent = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
 
 train_factor = 1
@@ -33,8 +33,8 @@ def calculate_fitness(population):
     fitness = np.empty((POPULATION_SIZE, 3))
 
     for i in range(POPULATION_SIZE):
-        error = get_errors(SECRET_KEY, list(population[i]))
-        # error = [10000000000, 1000000000]
+        # error = get_errors(SECRET_KEY, list(population[i]))
+        error = [10000000000, 1000000000]
         fitness[i][0] = error[0]
         fitness[i][1] = error[1]
         fitness[i][2] = error[0]*train_factor + error[1]
@@ -102,6 +102,7 @@ def main():
     #     population_fitness = np.array(json.loads(f.read())['population'])
     # print(population_and_fitness[0])
     num_generations = 5
+    offset = 0
 
     if where_json(FILE_NAME):
         with open(FILE_NAME) as json_file:
@@ -109,6 +110,7 @@ def main():
             population = [dict_item["Vector"] for dict_item in data["Storage"][-100:]]
             train = [dict_item["Train Error"] for dict_item in data["Storage"][-100:]]
             valid = [dict_item["Validation Error"] for dict_item in data["Storage"][-100:]]
+            offset = data["Storage"][-1]["Generation"]
             # fitness = [dict_item["Fitness"] for dict_item in data["Storage"][-100:]]
             # print(fitness)
             # print(train, valid)
@@ -138,7 +140,7 @@ def main():
                 data = json.load(json_file)
                 temporary = data["Storage"]
                 # print(i)
-                rowDict = { "Generation": 57 + generation, 
+                rowDict = { "Generation": generation + offset, 
                             "Vector": population[i].tolist(), 
                             "Train Error": fitness[i][0], 
                             "Validation Error": fitness[i][1],

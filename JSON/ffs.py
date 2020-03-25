851509c48c8b1bf2507f49fed2a8daf78ff45678
Shradha <shradha.sgl@gmail.com>
2020-03-25 20:38:32 +0530

    Panic

--- JSON/ffs.py
+++ JSON/ffs.py
@@ -49,26 +49,28 @@ if __name__ == "__main__":
     Replace "test" with your secret ID and just run this file 
     to verify that the server is working for your ID.
     """
-    # key = 'I22KGMKf3ZqtxxvxklykgAlk1dQZvVqhgfZT1i8NWjOgBC4ntl'
+    key = 'I22KGMKf3ZqtxxvxklykgAlk1dQZvVqhgfZT1i8NWjOgBC4ntl'
+    
+    
     vec = [
-                -6.492632799399137,
-                9.958672884973677,
-                -6.095379162998791,
-                0.06168095408181494,
-                0.037917049989923374,
-                8.507648718298185e-05,
-                -6.016147135503486e-05,
-                -1.2766030001770375e-07,
-                3.486376130484902e-08,
-                3.914021279922079e-11,
-                -6.706312927923882e-12
-            ]
+        0.0, 
+    0.12945764586495642, 
+    -6.039810271058455, 
+    0.0615251831403646,
+    0.036332767382739536, 
+    8.041602949621486e-05, 
+    -5.958473916150738e-05, 
+    -1.3376793283227787e-07, 
+    3.546145059600655e-08, 
+    4.407440521743608e-11, 
+    -6.941006728657045e-12
+    ]
     # vector = [0e+00,-2e-03,-8e-03,1e-02,-3e-07,3e-12,-1e-09,4e-13,-3e-13,1e-12,-3e-16]
     # vector = [ i * (1+ np.random.uniform(-1,1,1)[0]) for i in vector]
-    err = get_errors('dnLVLTHPAUOT2R1Ruj1sQvXxWBZZchp8u4WkyZGzaeTQCpyFXC', vec)
+    err = get_errors(key , vec)
     print(err, err[0] + err[1])
     assert len(err) == 2
 
-    # submit_status = submit('dnLVLTHPAUOT2R1Ruj1sQvXxWBZZchp8u4WkyZGzaeTQCpyFXC', list(-np.arange(0,1.1,0.1)))
+    # submit_status = submit(key, vec)
     # assert "submitted" in submit_status
     
--- final.py
+++ final.py
@@ -8,11 +8,11 @@ import os
 POPULATION_SIZE = 40
 VECTOR_SIZE = 11
 MATING_POOL_SIZE = 15
-FILE_NAME_READ = 'JSON/new.json'
-FILE_NAME_WRITE = 'JSON/new.json'
+FILE_NAME_READ = 'JSON/shradha.json'
+FILE_NAME_WRITE = 'JSON/shradha.json'
 first_parent = [0.0, 0.1240317450077846, -6.211941063144333, 0.04933903144709126, 0.03810848157715883, 8.132366097133624e-05, -6.018769160916912e-05, -1.251585565299179e-07, 3.484096383229681e-08, 4.1614924993407104e-11, -6.732420176902565e-12]
 
-train_factor = 1
+train_factor = -1
 fieldNames = ['Generation','Vector','Train Error','Validation Error', 'Fitness']
 
 def where_json(fileName):
@@ -38,7 +38,7 @@ def calculate_fitness(population):
         # error = [10000000000, 1000000000]
         fitness[i][0] = error[0]
         fitness[i][1] = error[1]
-        fitness[i][2] = error[0]*train_factor + error[1]
+        fitness[i][2] = abs(error[0]*train_factor + error[1])
         # fitness[i] = 6
 
     pop_fit = np.column_stack((population, fitness))
@@ -133,7 +133,7 @@ def main():
     # with open('clean_36.json','r+') as f:
     #     population_fitness = np.array(json.loads(f.read())['population'])
     # print(population_and_fitness[0])
-    num_generations = 2
+    num_generations = 1
     # offset = 0
 
     if where_json(FILE_NAME_READ):
@@ -146,7 +146,7 @@ def main():
             # fitness = [dict_item["Fitness"] for dict_item in data["Storage"][-100:]]
             # print(fitness)
             # print(train, valid)
-            fitness = [(train_factor*train[i] + valid[i]) for i in range(40)]
+            fitness = [abs(train_factor*train[i] + valid[i]) for i in range(40)]
             population_fitness = np.column_stack((population, train, valid, fitness))
             population_fitness = population_fitness[np.argsort(population_fitness[:,-1])]
             # print(population_fitness[:,-1])
